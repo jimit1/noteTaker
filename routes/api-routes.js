0847768da5fdce93cc97e7146c2a52c9d5da7d93
Jimit Mehta <jimitmehta@MacBook-Pro.local>
2020-06-24 15:50:32 -0700

    trying to fix delete

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -27,16 +27,14 @@ router.post("/api/notes", (req, res) => {
 });
 
 router.delete("/api/notes/:id", (req, res) => {
-  let data = fs.readFileSync("./db/db.json", "utf8");
-  data = JSON.parse(data);
-  let newId = req.params.id;
+  const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  const incomingID = req.params.id;
   data.forEach((value, index) => {
     if (value.id == incomingID) {
       data.splice(index, 1);
     }
   });
   fs.writeFileSync("./db/db.json", JSON.stringify(data, null, 2));
-  res.json({ msg: "Successfully deleted the todo" });
 });
 
 module.exports = router;
